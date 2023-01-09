d4051c2731b95638c4c789bbb08bb3b82a8246ca
vortex2jm <joaopaulomoura5328@gmail.com>
2023-01-09 12:13:28 -0300

    :recycle: Fixing config error

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,8 +1,10 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { Content } from "src/content/entities/content.entity";
+import { Trail } from "src/trail/entities/trail.entity";
 
 export default (): TypeOrmModuleOptions => ({
     type: 'sqlite',
     database: './src/database/database.sqlite',
-    entities: [__dirname + '*/**/*.entity{.ts,.js}'],
-    synchronize: true,
+    entities: [Trail, Content],
+    synchronize: true,  // Change this property to false when you build app for production
 })
