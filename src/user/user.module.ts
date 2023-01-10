f56d091cee4e42696affdba401ca8b2aa7ca7fa0
vortex2jm <joaopaulomoura5328@gmail.com>
2023-01-10 14:48:49 -0300

    :sparkles: Adding middleware consumer

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,13 +1,20 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { ApiKeyMiddleware } from 'src/middleware/api-key.middleware';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, ConfigService],
   exports: [UserService],
 })
-export class UserModule {}
+export class UserModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ApiKeyMiddleware)
+    .forRoutes({path: "user/register-admin", method: RequestMethod.POST})
+  }
+}
