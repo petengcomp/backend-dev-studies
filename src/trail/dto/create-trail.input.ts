21e5f67206815dbb41f91910a5e5038f4462b39e
vortex2jm <joaopaulomoura5328@gmail.com>
2023-04-03 14:57:34 -0300

    :recycle: Refactoring trail resource

--- src/trail/dto/create-trail.input.ts
+++ src/trail/dto/create-trail.input.ts
@@ -1,28 +1,8 @@
 import { Field, InputType } from '@nestjs/graphql';
-import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
-import { TrailType } from '../entities/trail.entity';
 
 @InputType()
 export class CreateTrailInput {
   
-  @IsString()
-  @IsNotEmpty({ message: 'Esse campo não pode ficar vazio' })
+  @Field({nullable: false})
   name: string;
-
-  @IsEnum(TrailType, {
-    message:
-      'Únicas opções aceitadas nesse campo: iniciantes/frontend/backend/devops',
-  })
-  @IsNotEmpty({
-    message: 'Esse campo não pode ficar vazio',
-  })
-  type: TrailType;
-
-  @IsNotEmpty({ message: 'Esse campo não pode ficar vazio' })
-  stack: string;
-
-  @IsNotEmpty({ message: 'Esse campo não pode ficar vazio' })
-  description: string;
 }
-
-// Suggestions: Add @Field() decorator and check if these decorators are working fine 
